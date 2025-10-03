6d2d524e0e029f9892a32aee32c9de9bea961989
glaubereduardoss <glauberdudu71@gmail.com>
2025-10-03 11:51:59 -0300

    Update e8.v

--- e8.v
+++ e8.v
@@ -4,11 +4,12 @@ module e8( A, Y, B);
 	output [3:0]A;
 	
 	and(A[0], B[0], Y[0]);
-   and(A[1], B[1], Y[1]);
-   and(A[2], B[2], Y[2]);
-   and(A[3], B[3], Y[3]);
+	and(A[1], B[1], Y[1]);
+	and(A[2], B[2], Y[2]);
+	and(A[3], B[3], Y[3]);
 	and(A[0], B[0], Y[0]);
-   and(A[1], B[1], Y[1]);
-   and(A[2], B[2], Y[2]);
-   and(A[3], B[3], Y[3]);
+	and(A[1], B[1], Y[1]);
+	and(A[2], B[2], Y[2]);
+	and(A[3], B[3], Y[3]);
 endmodule
+
